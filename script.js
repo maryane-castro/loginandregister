e223ac65490b580d706eecbda9baf8c8215cfc96
maryane-castro <maryane.castro993@gmail.com>
2023-03-13 12:14:07 -0300

    finally

--- script.js
+++ script.js
@@ -1,22 +1,77 @@
 const capa = document.querySelector('.capa')
 const loginPag = document.querySelector('.register-link')
 const registerPag = document.querySelector('.login-link')
-const x = document.querySelector('.icon-close')
+
+const btnPopup = document.querySelector('.btnLogin-popup')
+const btnClose = document.querySelector('.icon-close')
 
 
 
-x.addEventListener("click", () => {
-    
-})
 
 
 loginPag.addEventListener("click", () => {
     capa.classList.add('active')
 
 })
+
 registerPag.addEventListener("click", () => {
     capa.classList.remove('active')
 })
+
+btnPopup.addEventListener("click", () => {
+    capa.classList.add('active-popup')
+})
+
+
+btnClose.addEventListener("click", () => {
+    capa.classList.remove('active-popup')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -24,16 +79,9 @@ registerPag.addEventListener("click", () => {
 
 /*
 loginPag.addEventListener("click", () => {
-
 })
-
-
-
-
-
 function register(){
     console.log('ativado')
 }
-
 registro.addEventListener("click", register)
 */
